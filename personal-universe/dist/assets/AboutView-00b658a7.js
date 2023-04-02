4c512a826de55959f276e895d1bb43eb317a3313
Olga Misevich <olga@clear-canvas.com>
2023-04-02 19:35:18 +0200

    compiled

---
+++ personal-universe/dist/assets/AboutView-00b658a7.js
@@ -0,0 +1,1 @@
+import{_ as t,o,c as s,a as e}from"./index-2963362b.js";const _={},c={class:"about"},n=e("h1",null,'О создателях "Твоей Вселенной"',-1),a=e("p",null," ??? ",-1),r=[n,a];function i(l,u){return o(),s("section",c,r)}const f=t(_,[["render",i]]);export{f as default};
